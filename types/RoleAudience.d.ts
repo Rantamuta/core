967cafbc31650a8eff6bd460b75fce64f1a5716b
Rendall <rendall@gmail.com>
2026-02-13 12:06:25 +0200

    types: tighten unknowns in RoleAudience.d.ts

--- types/RoleAudience.d.ts
+++ types/RoleAudience.d.ts
@@ -1,7 +1,11 @@
 export = RoleAudience;
 declare class RoleAudience extends ChannelAudience {
-    constructor(options: unknown);
-    minRole: unknown;
+    constructor(options: RoleAudienceOptions);
+    minRole: number;
     getBroadcastTargets(): ReturnType<ChannelAudience["getBroadcastTargets"]>;
 }
 import ChannelAudience = require("./ChannelAudience");
+type RoleAudienceOptions = {
+    minRole: number;
+    [key: string]: unknown;
+};
