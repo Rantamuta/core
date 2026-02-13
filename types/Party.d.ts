1f62bf34a1f44a648bc81f03fc0b647f2d3e8edb
Rendall <rendall@gmail.com>
2026-02-13 12:02:04 +0200

    types: tighten unknowns in Party.d.ts

--- types/Party.d.ts
+++ types/Party.d.ts
@@ -2,13 +2,14 @@ export = Party;
 /**
  * Representation of an adventuring party
  */
-declare class Party extends Set<unknown> {
-    constructor(leader: unknown);
-    invited: Set<unknown>;
-    leader: unknown;
+declare class Party extends Set<Character> {
+    constructor(leader: Character);
+    invited: Set<Character>;
+    leader: Character;
     disband(): void;
-    invite(target: unknown): void;
-    isInvited(target: unknown): boolean;
-    removeInvite(target: unknown): void;
-    getBroadcastTargets(): unknown[];
+    invite(target: Character): void;
+    isInvited(target: Character): boolean;
+    removeInvite(target: Character): void;
+    getBroadcastTargets(): Character[];
 }
+import Character = require("./Character");
