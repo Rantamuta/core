d96453b2c98087d42e1ce830159a5ca9ad2c8af2
Rendall <rendall@gmail.com>
2026-02-13 12:00:48 +0200

    types: tighten unknowns in Metadatable.d.ts

--- types/Metadatable.d.ts
+++ types/Metadatable.d.ts
@@ -5,4 +5,13 @@ export = Metadatable;
  * @param {*} parentClass
  * @return {module:MetadatableFn~Metadatable}
  */
-declare function Metadatable(parentClass: unknown): unknown;
+declare function Metadatable<TBase extends Constructor<HasMetadata>>(parentClass: TBase): TBase & Constructor<MetadatableInstance>;
+type Constructor<T = object> = new (...args: unknown[]) => T;
+type HasMetadata = {
+    metadata: Record<string, unknown>;
+    emit(eventName: string, ...args: unknown[]): unknown;
+};
+type MetadatableInstance = {
+    setMeta(key: string, value: unknown): void;
+    getMeta(key: string): unknown;
+};
