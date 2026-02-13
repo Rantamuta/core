89ba72dc7dbd92862e81b2ba562960705b9eb158
Rendall <rendall@gmail.com>
2026-02-13 12:05:10 +0200

    types: tighten unknowns in QuestReward.d.ts

--- types/QuestReward.d.ts
+++ types/QuestReward.d.ts
@@ -9,15 +9,16 @@ declare class QuestReward {
      * Assign the reward to the player
      * @param {GameState} GameState
      * @param {Quest} quest   quest this reward is being given from
-     * @param {object} config
+    * @param {object} config
      * @param {Player} player
      */
-    static reward(GameState: unknown, quest: Quest, config: object, player: Player): void;
+    static reward(GameState: GameState, quest: Quest, config: Record<string, unknown>, player: Player): void;
     /**
      * Render the reward
      * @return string
      */
-    static display(GameState: unknown, quest: unknown, config: unknown, player: unknown): void;
+    static display(GameState: GameState, quest: Quest, config: Record<string, unknown>, player: Player): string;
 }
+import GameState = require("./GameState");
 import Player = require("./Player");
 import Quest = require("./Quest");
