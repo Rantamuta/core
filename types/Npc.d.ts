99fcc9d9f6ff4551126b0b6eeb15a3b7ebe2653e
Rendall <rendall@gmail.com>
2026-02-13 12:01:34 +0200

    types: tighten unknowns in Npc.d.ts

--- types/Npc.d.ts
+++ types/Npc.d.ts
@@ -7,19 +7,19 @@ export = Npc;
  * @mixes Scriptable
  */
 declare class Npc extends Character {
-    constructor(area: unknown, data: unknown);
-    area: unknown;
-    script: unknown;
+    constructor(area: Area, data: NpcDefinition);
+    area: Area;
+    script?: string;
     behaviors: Map<string, unknown>;
-    equipment: Map<unknown, unknown>;
-    defaultEquipment: unknown;
-    defaultItems: unknown;
-    description: unknown;
-    entityReference: unknown;
-    id: unknown;
-    keywords: unknown;
-    quests: unknown;
-    uuid: unknown;
+    equipment: Map<string, Item>;
+    defaultEquipment: Record<string, string>;
+    defaultItems: string[];
+    description?: string;
+    entityReference: string;
+    id: string | number;
+    keywords: string[];
+    quests: string[];
+    uuid: string;
     commandQueue: CommandQueue;
     /**
      * Move the npc to the given room, emitting events appropriately
@@ -29,9 +29,27 @@ declare class Npc extends Character {
      * @fires Room#npcEnter
      * @fires Npc#enterRoom
      */
-    moveTo(nextRoom: Room, onMoved?: Function): void;
-    hydrate(state: unknown): void;
+    moveTo(nextRoom: Room, onMoved?: () => unknown): void;
+    hydrate(state: GameState): void;
 }
 import Character = require("./Character");
 import CommandQueue = require("./CommandQueue");
 import Room = require("./Room");
+import Area = require("./Area");
+import Item = require("./Item");
+import GameState = require("./GameState");
+type NpcDefinition = {
+    keywords: string[];
+    name: string;
+    id: string | number;
+    area: Area;
+    script?: string;
+    behaviors?: Record<string, unknown>;
+    equipment?: Record<string, string>;
+    items?: string[];
+    description?: string;
+    entityReference: string;
+    quests?: string[];
+    uuid?: string;
+    [key: string]: unknown;
+};
